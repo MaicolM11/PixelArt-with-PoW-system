2cc3360d51bcd339b487f9da54372f54cd7221f1
maicol <mmoarias4@gmail.com>
2021-05-02 18:03:24 -0500

    add start.sh

--- server/start.sh
+++ server/start.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 > logs/access.log
 rm -r task/*
+docker build -t server .
 docker rm -f $(docker container ls -aq)
-docker run --net=host -dti --env PORT=3001 --name Server3001  server
-docker run --net=host -dti --env PORT=3002 --name Server3002  server
-docker run --net=host -dti --env PORT=3003 --name Server3003  server
+for i in {1..3}; do
+  docker run --net=host -dti --env PORT=300$i --name Server300$i  server
+done
